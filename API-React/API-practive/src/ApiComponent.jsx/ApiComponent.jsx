8087dbef6af7f459dcbe2de251b65298431ea1d0
nguyenphuoccuong <cuong211094@gmail.com>
2024-10-18 15:32:13 +0700

    API

---
+++ API-React/API-practive/src/ApiComponent.jsx/ApiComponent.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+
+function ApiComponent () {
+  const [user,setUser] = useState ([])
+  
+  const [data,setdata] = useState ('')
+
+  const handleSearch = (e) => {
+    setdata(e.target.value);
+  }
+  
+  const filData = data === '0' ? user : data?
+  user.filter((user)=> user.id === parseInt(data)): user;
+  
+  useEffect (()=>{
+    fetch('https://jsonplaceholder.typicode.com/usershttps://jsonplaceholder.typicode.com/users')
+    .then((res)=>{
+      return res.json();
+    }).then((data)=>{
+      setUser(data)
+    })
+  },[])
+
+
+  return (
+      <div>
+          <input type="text" name="search" placeholder='Search' onChange={handleSearch}/>
+          <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>User</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Phone</th>
+                </tr>
+            </thead>
+            <tbody>
+              {
+                filData.map((value,key)=>(
+                  <tr key={key}>
+                    <td>{value.id}</td>
+                    <td>{value.username}</td>
+                    <td>{value.email}</td>
+                    <td>{value.address.street}</td>
+                    <td>{value.phone}</td>
+                  </tr>
+                ))
+              }
+                
+            </tbody>
+          </Table>
+      </div>  
+    )
+}
+
+export default ApiComponent;
---
+++ API-React/API-practive/src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ApiComponent from './ApiComponent.jsx/ApiComponent'
+import Table from 'react-bootstrap/Table';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <ApiComponent/>
+    </>
+  )
+}
+
+export default App
